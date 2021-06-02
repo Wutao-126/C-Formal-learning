ae2901f9750fa179b9257c0b499fc9439475f23b
wutao <1260569390@qq.com>
2021-06-02 21:49:39 +0800

    初识c

---
+++ 6_2practice/test.c
@@ -0,0 +1,45 @@
+# define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("hello world!\n");
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int age = 10;
+//	int price = 50;
+//	double weight = 55.5;
+//	return 0;
+//}
+
+
+int main()
+{
+	//如何计算一个类型创建的变量所占空间大大小
+	//sizeof();
+	//%d - 整数
+	//%c - 字符
+	printf("%d", sizeof(char));
+	printf("%d", sizeof(short));
+	printf("%d", sizeof(int));
+	printf("%d", sizeof(long));
+	printf("%d", sizeof(long long));
+	printf("%d", sizeof(float));
+	printf("%d", sizeof(double));
+	return 0;
+}
+
+
+
+
+
+//main()
+//{
+//	int x, y;
+//	scanf("x=%d,y=%d", &x, &y);
+//	printf("x+y=%d", x + y);
+//}
