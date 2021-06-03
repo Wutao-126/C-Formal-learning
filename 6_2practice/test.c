eb888887630e511154aff2ca4183da803b984279
wutao <1260569390@qq.com>
2021-06-03 20:51:25 +0800

    modify

--- 6_2practice/test.c
+++ 6_2practice/test.c
@@ -17,24 +17,39 @@
 //}
 
 
+//int main()
+//{
+//	//如何计算一个类型创建的变量所占空间大大小
+//	//sizeof();
+//	//%d - 整数
+//	//%c - 字符
+//	printf("%d", sizeof(char));
+//	printf("%d", sizeof(short));
+//	printf("%d", sizeof(int));
+//	printf("%d", sizeof(long));
+//	printf("%d", sizeof(long long));
+//	printf("%d", sizeof(float));
+//	printf("%d", sizeof(double));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    printf("I lost my cellphone!");
+//    return 0;
+//}
+
+
+#include<stdio.h>
 int main()
 {
-	//如何计算一个类型创建的变量所占空间大大小
-	//sizeof();
-	//%d - 整数
-	//%c - 字符
-	printf("%d", sizeof(char));
-	printf("%d", sizeof(short));
-	printf("%d", sizeof(int));
-	printf("%d", sizeof(long));
-	printf("%d", sizeof(long long));
-	printf("%d", sizeof(float));
-	printf("%d", sizeof(double));
-	return 0;
+    int a = 40;
+    int c = 212;
+    printf("%d", ((-8 + 22) * a - 10 + c / 2));
+    return 0;
 }
-
-
-
 
 
 //main()
